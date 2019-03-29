4c781bf4e88eebc8e334066a823b4ae1c4c10b73
tokushima <tokushima@192.168.11.19>
2019-03-29 20:41:14 +0900

    add

--- lib/tt/linebot/Bot.php
+++ lib/tt/linebot/Bot.php
@@ -3,155 +3,131 @@ namespace tt\linebot;
 
 class Bot{
 	private $bot;
-	private $message;
 	private $access_token;
+	private $vars = [];
+	
+	const EVENT_TYPE_TEXT = 1;
+	const EVENT_TYPE_STICKER = 2;
+	const EVENT_TYPE_IMAGE = 3;
+	const EVENT_TYPE_POSTBACK = 4;
+	
+	const EVENT_TYPE_FOLLOW = 11;
+	const EVENT_TYPE_UNFOLLOW = 11;
+	
 	
 	public static function client(){
 		$access_token = \ebi\Conf::get('access_token');
 		$secret = \ebi\Conf::get('secret');
 		
-		return new self($access_token,$secret);		
+		return new self($access_token,$secret);
 	}
 	
-	public function __construct($access_token,$secret){		
+	public function __construct($access_token,$secret){
 		$this->access_token = $access_token;
 		$this->bot = new \LINE\LINEBot(
-			new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token),
-			['channelSecret'=>$secret]
-		);
-		
-		$this->message = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
+				new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token),
+				['channelSecret'=>$secret]
+				);
 	}
 	
 	/**
-	 * 
-	 * @return \tt\linebot\Event
+	 *
+	 * @return \LINE\LINEBot\Event\BaseEvent
 	 */
 	public function get_events(){
 		$post_input = file_get_contents('php://input');
 		
-		$events = $this->bot->parseEventRequest(
-			$post_input,
-			(new \ebi\Env())->get('HTTP_'.\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE)
-		);
-		
-		foreach($events as $event){
-			yield new \tt\linebot\Event($event);
-		}
+		return $this->bot->parseEventRequest(
+				$post_input,
+				(new \ebi\Env())->get('HTTP_'.\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE)
+				);
 	}
 	
+	
+	public function get_type(\LINE\LINEBot\Event\BaseEvent $event){
+		if($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage){
+			return self::EVENT_TYPE_TEXT;
+		}
+		if($event instanceof \LINE\LINEBot\Event\MessageEvent\StickerMessage){
+			return self::EVENT_TYPE_STICKER;
+		}
+		if($event instanceof \LINE\LINEBot\Event\MessageEvent\ImageMessage){
+			return self::EVENT_TYPE_IMAGE;
+		}
+		if($event instanceof \LINE\LINEBot\Event\PostbackEvent){
+			return self::EVENT_TYPE_POSTBACK;
+		}
+		if($event instanceof \LINE\LINEBot\Event\FollowEvent){
+			return self::EVENT_TYPE_FOLLOW;
+		}
+		if($event instanceof \LINE\LINEBot\Event\UnfollowEvent){
+			return self::EVENT_TYPE_UNFOLLOW;
+		}
+		return null;
+	}
 	
 	public function get_content($message_id){
 		return $this->bot->getMessageContent($message_id)->getRawBody();
 	}
 	
-	
-	public function add_text($text){
-		$this->message->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+	public function get_text(\LINE\LINEBot\Event\BaseEvent $event){
+		return trim($event->getText());
 	}
-		
-	/**
-	 * 
-	 * @param integer $package_id
-	 * @param integer $sticker_id
-	 * @see https://developers.line.me/media/messaging-api/messages/sticker_list.pdf
-	 */
-	public function add_sticker($package_id,$sticker_id){
-		$this->message->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($package_id,$sticker_id));
-	}
-	
-	public function add_image($original_url,$preview_url=null){
-		if(empty($preview_url)){
-			$preview_url = $original_url;
+	public function in_vars(\LINE\LINEBot\Event\BaseEvent $event,$key,$default=null){
+		if(!isset($this->vars[$event->getReplyToken()])){
+			parse_str($event->getPostbackData(),$this->vars[$event->getReplyToken()]);
 		}
-		$this->message->add(new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($original_url,$preview_url));
+		return $this->vars[$key] ?? $default;
+	}
+	public function get_sticker_id(\LINE\LINEBot\Event\BaseEvent $event){
+		return [$event->getPackageId(),$event->getStickerId()];
+	}
+	public function get_message_id(\LINE\LINEBot\Event\BaseEvent $event){
+		return $event->getMessageId();
 	}
 	
-	public function add_confirm($alt,$text,array $vars){
-		$buttons = [];
-		foreach($vars as $k => $v){
-			$buttons[] = $this->parse_action($k,$v);
-		}
-		
-		$this->message->add(new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
-			($alt ?? $text),
-			new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder(
-				$text,
-				$buttons
-			)
-		));
-	}
-	
-	public function add_button($alt,$column_var){
-		$buttons = [];
-		
-		foreach(($column_var['buttons'] ?? []) as $bk => $bv){
-			$buttons[] = $this->parse_action($bk,$bv);
-		}
-		$this->message->add(new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
-			$alt,
-			new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder(
-				$column_var['title'] ?? null,
-				$column_var['text'] ?? null,
-				$column_var['image'] ?? null,
-				$buttons
-			)
-		));
-	}
-	
-	public function add_carousel($alt,$column_vars){
-		$columns = [];
-		foreach($column_vars as $column_var){
-			$buttons = [];
-			
-			foreach(($column_var['buttons'] ?? []) as $bk => $bv){
-				$buttons[] = $this->parse_action($bk,$bv);
-			}
-			$columns[] = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder(
-				$column_var['title'] ?? null,
-				$column_var['text'] ?? null,
-				$column_var['image'] ?? null,
-				$buttons
-			);
-		}
-		$this->message->add(new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder(
-			$alt,
-			new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns)
-		));
-	}
-		
-	private function parse_action($title,$vars_or_url){
-		if(is_array($vars_or_url)){
-			return new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder(
-				$title,
-				http_build_query($vars_or_url)
-			);
-		}
-		return new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder($title,$vars_or_url);
-	}
-	
-	
-	public function reply(\tt\linebot\Event $event){
-		$this->bot->replyMessage($event->reply_token(),$this->message);
-	}
-	
-	public function reply_json(\tt\linebot\Event $event,$json){
-		if(is_string($json)){
-			$json = \ebi\Json::decode(\ebi\Util::plain_text($json));
-		}
-		
-		$request = [
+	public function reply(\tt\linebot\Event $event,$messages){
+		$this->send('https://api.line.me/v2/bot/message/reply',[
 			'replyToken'=>$event->reply_token(),
-			'messages'=>$json,
-		];
-				
+			'messages'=>$this->get_message_vars($messages),
+		]);
+	}
+	
+	public function multicast($tos,$messages){
+		if(!is_array($tos)){
+			$tos = [$tos];
+		}
+		if(sizeof($tos) > 150){
+			throw new \ebi\exception\MaxSizeExceededException('max 150, input '.sizeof($tos));
+		}
+		
+		$this->send('https://api.line.me/v2/bot/message/multicast',[
+			'to'=>$tos,
+			'messages'=>$this->get_message_vars($messages),
+		]);
+	}
+	
+	public function push($to,$messages){
+		$this->send('https://api.line.me/v2/bot/message/push',[
+			'to'=>$to,
+			'messages'=>$this->get_message_vars($messages),
+		]);
+	}
+	
+	private function get_message_vars($messages){
+		if(is_string($messages)){
+			$messages = \ebi\Json::decode(\ebi\Util::plain_text($messages));
+		}
+		return $messages;
+	}
+	private function send($url,array $request){
 		$b = new \ebi\Browser();
 		$b->bearer_token($this->access_token);
 		$b->header('Content-Type','application/json');
-		$b->do_raw('https://api.line.me/v2/bot/message/reply',json_encode($request));
+		$b->do_raw($url,json_encode($request));
 		
 		if($b->status() !== 200){
-			throw new \ebi\exception\InvalidArgumentException($b->body());
+			throw new \ebi\exception\InvalidArgumentException($b->status().': '.$b->body());
 		}
 	}
 }
